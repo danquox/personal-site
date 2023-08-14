1a3767bb475fe6764c19e4d05e4723254dfa646e
dang <dt122002@gmail.com>
2023-08-13 21:06:05 -0400

    sizing fix

--- src/pages/music.tsx
+++ src/pages/music.tsx
@@ -6,7 +6,7 @@ import { BsArrowRightShort, BsArrowLeftShort } from "react-icons/bs"
 
 export default function Music() {
   return (
-    <div className="flex flex-col gap-4 w-screen md:w-full p-8">
+    <div className="flex flex-col gap-4 w-screen md:w-full p-8 overflow-x-hidden">
       <Link className="flex items-center text-white/60 hover:text-white/80 bg-white/10 hover:bg-white/20 pr-2 p-1 rounded-md w-fit mr-auto" href={"/"}>
         <BsArrowLeftShort className="w-5 h-5" />
         <span>back</span>
